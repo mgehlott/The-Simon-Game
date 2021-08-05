a4fc3b716132064351766ef915045a3483f759bb
Mahendra <mahendragehlot005@gmail.com>
2021-08-05 09:15:53 +0530

    nextSequence() function added in game.js

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-$("h1").html("jquery is added");
+const buttonColors = ["red","blue","green","yellow"];
+var patternArray=[];
+
+var  userClickedArray=[];
+$(".btn").click(function(){
+    var userbuttonClickedColor = $(this).attr("id");
+    userClickedArray.push(userbuttonClickedColor);
+})
+
+function nextSequence()
+{
+    var randomNumber = Math.floor(Math.random()*4);
+    var choosenColor = buttonColors[randomNumber];
+    patternArray.push(choosenColor);
+
+    var btn = $("#"+choosenColor);
+
+    btn.fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + choosenColor + ".mp3");
+    audio.play();
+
+
+    
+}
+
+nextSequence();
